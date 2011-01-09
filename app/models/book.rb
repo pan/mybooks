969de15af2c307ee8e014114eff86f4bdfc795d0
Pan GaoYong <pan.gaoyong@gmail.com>
2011-01-09 17:58:49 +0800

    reject blank nested attributes and list them on index page

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ActiveRecord::Base
   has_many :cds
-  accepts_nested_attributes_for :cds, :allow_destroy => true
+  accepts_nested_attributes_for :cds, :allow_destroy => true, 
+    :reject_if => :all_blank
 end
